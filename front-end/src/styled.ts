dd76690fd821753340d0f28f20466f2441d61eed
epickaowca <owcaofficial@yahoo.com>
2023-12-25 17:44:07 +0100

    commit

--- front-end/src/styled.ts
+++ front-end/src/styled.ts
@@ -1,5 +1,21 @@
 import { createGlobalStyle, css } from "styled-components";
 
+export const myTheme = {
+  moderateBlue: "hsl(238, 40%, 52%)",
+  softRed: "hsl(358, 79%, 66%)",
+  lightGrayishBlue: "hsl(239, 57%, 85%)",
+  paleRed: "hsl(357, 100%, 86%)",
+  darkBlue: "hsl(212, 24%, 26%)",
+  grayishBlue: "hsl(211, 10%, 45%)",
+  lightGray: "hsl(223, 19%, 93%)",
+  veryLightGray: "hsl(228, 33%, 97%)",
+} as const;
+
+type MyTheme = typeof myTheme;
+declare module "styled-components" {
+  export interface DefaultTheme extends MyTheme {}
+}
+
 export const GlobalStyle = createGlobalStyle(
   ({ theme }) => css`
     @import url("https://fonts.googleapis.com/css2?family=Rubik:wght@400;500;700&display=swap");
@@ -44,19 +60,3 @@ export const GlobalStyle = createGlobalStyle(
     }
   `
 );
-
-export const myTheme = {
-  moderateBlue: "hsl(238, 40%, 52%)",
-  softRed: "hsl(358, 79%, 66%)",
-  lightGrayishBlue: "hsl(239, 57%, 85%)",
-  paleRed: "hsl(357, 100%, 86%)",
-  darkBlue: "hsl(212, 24%, 26%)",
-  grayishBlue: "hsl(211, 10%, 45%)",
-  lightGray: "hsl(223, 19%, 93%)",
-  veryLightGray: "hsl(228, 33%, 97%)",
-} as const;
-
-type MyTheme = typeof myTheme;
-declare module "styled-components" {
-  export interface DefaultTheme extends MyTheme {}
-}
