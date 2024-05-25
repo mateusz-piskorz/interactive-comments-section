20711b8a3cd9dd8faeb5666b9559396b971554a7
Epicka Owca <epickaowca@Epickas-Laptop.local>
2024-05-25 11:59:40 +0200

    Comment - update unit tests

--- front-end/src/features/Comment/Comment.spec.tsx
+++ front-end/src/features/Comment/Comment.spec.tsx
@@ -1,40 +1,39 @@
-export {};
-// import { screen, render, waitFor } from "@testing-library/react";
-// import { Comment } from "./index";
-// import { user, comment1, comment2 } from "../../../tests/constants";
-// import { Post } from "./components/Post";
-// import { useEffect } from "react";
+import { screen, render, waitFor } from "@testing-library/react";
+import { Comment } from "./index";
+import { comment1, comment2 } from "../../../tests/constants";
+import { Post } from "./components/Post";
+import { useEffect } from "react";
 
-// jest.mock("./components/Post", () => ({
-//   Post: jest.fn(({ commentId }) => <h1>{commentId}</h1>),
-// }));
+jest.mock("./components/Post", () => ({
+  Post: jest.fn(({ commentId }) => <h1>{commentId}</h1>),
+}));
 
-// jest.mock("../Form", () => ({
-//   Form: jest.fn(() => <h1>Form</h1>),
-// }));
+jest.mock("../Form", () => ({
+  Form: jest.fn(() => <h1>Form</h1>),
+}));
 
-// it("displays Post", () => {
-//   render(<Comment commentId={comment1._id} nestingLevel={0} />);
-//   expect(screen.getByText(comment1._id)).toBeInTheDocument();
-// });
+it("displays Post", () => {
+  render(<Comment commentId={comment1.id} nestingLevel={0} />);
+  expect(screen.getByText(comment1.id)).toBeInTheDocument();
+});
 
-// it("displays Posts children", () => {
-//   render(<Comment commentId={comment1._id} nestingLevel={0} />);
-//   expect(screen.getByText(comment2._id)).toBeInTheDocument();
-// });
+it("displays Posts children", () => {
+  render(<Comment commentId={comment1.id} nestingLevel={0} />);
+  expect(screen.getByText(comment2.id)).toBeInTheDocument();
+});
 
-// it("doesn't display Form by default", async () => {
-//   render(<Comment commentId={comment2._id} nestingLevel={0} />);
-//   expect(screen.queryByText("Form")).toBeNull();
-// });
+it("doesn't display Form by default", async () => {
+  render(<Comment commentId={comment2.id} nestingLevel={0} />);
+  expect(screen.queryByText("Form")).toBeNull();
+});
 
-// it("displays Form onReply function call", async () => {
-//   (Post as jest.Mock<any>).mockImplementation(({ onReply }) => {
-//     useEffect(() => {
-//       onReply();
-//     }, []);
-//     return <h1>Post</h1>;
-//   });
-//   render(<Comment commentId={comment2._id} nestingLevel={0} />);
-//   expect(screen.getByText("Form")).toBeInTheDocument();
-// });
+it("displays Form onReply function call", async () => {
+  (Post as jest.Mock<any>).mockImplementation(({ onReply }) => {
+    useEffect(() => {
+      onReply();
+    }, []);
+    return <h1>Post</h1>;
+  });
+  render(<Comment commentId={comment2.id} nestingLevel={0} />);
+  expect(screen.getByText("Form")).toBeInTheDocument();
+});
